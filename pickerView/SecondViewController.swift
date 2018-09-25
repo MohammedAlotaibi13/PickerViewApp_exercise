509858cda4d5fe09542d557f1be2c8cd56200486
Mohammed alotaibi <muhammedalotaibi13@gmail.com>
2018-09-25 17:56:59 +0300

    intial commit

---
+++ pickerView/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  pickerView
+//
+//  Created by محمد عايض العتيبي on 14/01/1440 AH.
+//  Copyright © 1440 code schoole. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+}
---
+++ pickerView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pickerView
+//
+//  Created by محمد عايض العتيبي on 14/01/1440 AH.
+//  Copyright © 1440 code schoole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+ 
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var pickerView: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func nextButton(_ sender: UIButton) {
+    }
+    
+    
+
+}
+
